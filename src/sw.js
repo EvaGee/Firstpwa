4524127e292eaa6111948a516e276cdab33b93ea
kinotikennedy@gmail.com <kinotikennedy@gmail.com>
2019-08-06 09:24:57 +0300

    Add existing file

--- src/sw.js
+++ src/sw.js
@@ -31,10 +31,7 @@ self.addEventListener('fetch', function(event) {
                 return response;
               }
   
-              // IMPORTANT: Clone the response. A response is a stream
-              // and because we want the browser to consume the response
-              // as well as the cache consuming the response, we need
-              // to clone it so we have two streams.
+        
               var responseToCache = response.clone();
   
               caches.open(CACHE_NAME)
@@ -48,3 +45,4 @@ self.addEventListener('fetch', function(event) {
         })
       );
   });
+  
